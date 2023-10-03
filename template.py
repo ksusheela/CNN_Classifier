a7c7c9c16f6468ab3929e1799efaf27f52ea1ba8
Susheela kothakonda <sushe9sushe@gmail.com>
2023-10-03 02:09:53 +0200

    template updated

--- template.py
+++ template.py
@@ -24,7 +24,7 @@ list_of_files =[
     "init_setup.sh",
     "requirements.txt",
     "requirements_dev.txt",
-    "setup.py"
+    "setup.py",
     "setup.cfg",
     "pyproject.toml",
     "tox.ini",
