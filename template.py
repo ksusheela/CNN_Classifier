3ee8e533eb65116a489d6a14c74789f2b1555664
Susheela kothakonda <sushe9sushe@gmail.com>
2023-10-03 02:01:45 +0200

    template updated

--- template.py
+++ template.py
@@ -31,5 +31,20 @@ list_of_files =[
     "research/trials.ipynb", 
 ]
 
+
 for filepath in list_of_files:
-    path(filepath)
+    filepath=Path(filepath)
+    filedir,filename=os.path.split(filepath)
+    
+    if filedir !="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"creating directory: {filedir} for the file: {filename}")
+        
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,"w") as f:
+            pass
+            logging.info(f"creating empty file: {filepath}")
+    else:
+        logging.info(f"{filepath} already exists")
+
+   
